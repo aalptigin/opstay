05a83ee8d2ab5936f9e04c34e4e7a82ede3ce61e
Alp <seninmailin@ornek.com>
2025-12-25 23:34:18 +0300

    Fix /middleware runtime for Next 16

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { COOKIE_NAME } from "./lib/auth.cookie";
 
-export const runtime = "edge";
+export const runtime = "experimental-edge";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
