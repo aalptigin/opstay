4118bff273ce35502f8be0b78cd8335d4b7e3d05
Alp <seninmailin@ornek.com>
2025-12-25 23:14:03 +0300

    Fix middleware import path

--- app/middleware.ts
+++ app/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { COOKIE_NAME } from "./lib/auth.cookie";
+import { COOKIE_NAME } from "@/lib/auth.cookie";
 
 export const runtime = "edge";
 
