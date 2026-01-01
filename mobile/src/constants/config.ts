4fe864cfe7ac1ebd29a2b4c291b41e42a473518c
Alp <seninmailin@ornek.com>
2026-01-01 22:33:18 +0300

    Chore: remove mobile folder

--- mobile/src/constants/config.ts
+++
@@ -1,8 +0,0 @@
-// mobile/src/constants/config.ts
-export const CONFIG = {
-  // Lokal geliştirme için örnek:
-  // Android emulator: http://10.0.2.2:3000
-  // iOS simulator: http://localhost:3000
-  // Gerçek cihaz: PC IP'niz (ör: http://192.168.1.20:3000)
-  API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL || "http://10.0.2.2:3000",
-};
